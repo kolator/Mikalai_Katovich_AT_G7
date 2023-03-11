431aadf20fca59a443efc301c70d227e9c2e572b
Mikalai_Katovich <Mikalai_Katovich@gmail.com>
2023-03-11 10:38:16 +0100

    week 3 task 2 update

--- src/homework/week3/part2/StringTask_1_4.java
+++ src/homework/week3/part2/StringTask_1_4.java
@@ -1,38 +1,46 @@
 package homework.week3.part2;
 
-public class StringTask_1_4 {
-
-    public static void findAndConvertDigits(String text) {
-        int[] numbers = new int[text.length()]; // Создание массива для хранения чисел
-        int index = 0; // Индекс в массиве чисел
-
-        int numberStart = -1; // Позиция начала текущего числа
-        int number = 0; // Текущее число
-
-        for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            if (c >= '0' && c <= '9') { // Если символ является цифрой
-                if (numberStart == -1) { // Если мы еще не находимся внутри числа
-                    numberStart = i;
-                }
-                number = number * 10 + (c - '0'); // Увеличиваем текущее число на значение следующей цифры
-            } else { // Если символ не является цифрой
-                if (numberStart != -1) { // Если мы находимся внутри числа
-                    numbers[index] = number;
-                    index++;
-                    numberStart = -1;
-                    number = 0;
-                }
-            }
-        }
-
-        if (numberStart != -1) { // Если в конце строки находится число
-            numbers[index] = number;
-            index++;
-        }
-
-        for (int i = 0; i < index; i++) {
-            System.out.println(numbers[i]);
-        }
-    }
-}
+//public class StringTask_1_4 {
+//    public static void analyzeLog(String log) {
+//        StringBuilder ip = new StringBuilder(); // Переменная для хранения IP-адреса
+//        StringBuilder status = new StringBuilder(); // Переменная для хранения статуса доступа (granted или denied)
+//        boolean isIp = false; // Флаг для определения, что сейчас парсим IP-адрес
+//        boolean isStatus = false; // Флаг для определения, что сейчас парсим статус доступа
+//        int okCount = 0; // Счетчик удачных попыток доступа
+//        int failedCount = 0; // Счетчик неудачных попыток доступа
+//
+//        for (int i = 0; i < log.length(); i++) {
+//            char c = log.charAt(i); // Получаем текущий символ
+//
+//            if (Character.isDigit(c) || c == '.') { // Если символ является цифрой или точкой
+//                if (isStatus) { // Если мы уже получили статус доступа, значит это IP-адрес
+//                    ip.append(c); // Добавляем символ к IP-адресу
+//                } else { // Иначе это статус доступа
+//                    status.append(c); // Добавляем символ к статусу доступа
+//                }
+//            } else if (c == ' ') { // Если символ является пробелом
+//                if (!isIp) { // Если мы еще не получили IP-адрес
+//                    isIp = true; // Устанавливаем флаг
+//                } else { // Если мы уже получили IP-адрес, значит это статус доступа
+//                    isStatus = true; // Устанавливаем флаг
+//                }
+//            } else if (c == '\n') { // Если символ является символом новой строки
+//                if (status.toString().equals("granted")) { // Если статус доступа - granted
+//                    okCount++; // Увеличиваем счетчик удачных попыток
+//                } else { // Иначе статус доступа - denied
+//                    failedCount++; // Увеличиваем счетчик неудачных попыток
+//                }
+//
+//                System.out.println("ip " + ip.toString() + ": ok - " + okCount + ", failed - " + failedCount);
+//
+//                // Сбрасываем значения переменных для следующей строки лога
+//                ip.setLength(0);
+//                status.setLength(0);
+//                isIp = false;
+//                isStatus = false;
+//                okCount = 0;
+//                failedCount = 0;
+//            }
+//        }
+//    }
+//}
