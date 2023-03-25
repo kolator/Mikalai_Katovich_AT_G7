39304cfe18aa8f2e1c0f4149c625e48495456260
Mikalai_Katovich <Mikalai_Katovich@gmail.com>
2023-03-25 10:24:08 +0100

    update task 11 bubbleList

--- src/homework/week6/task11/BubbleList.java
+++ src/homework/week6/task11/BubbleList.java
@@ -4,33 +4,33 @@ package homework.week6.task11;
 import java.util.Arrays;
 import java.util.List;
 
-import project.properties.Bubble;
+import homework.week6.task00.Bubble;
 
 public class BubbleList {
     public static void main(String[] args) {
         // Создаем список пузырей
         List<Bubble> bubbles = Arrays.asList(
-//                new Bubble("CO2", 1),
-//                new Bubble("O2", 4),
-//                new Bubble("CO", 5)
+                new Bubble(1, "CO2"),
+                new Bubble(4, "O2"),
+                new Bubble(5, "CO")
         );
 
         // Итерируемся по списку и выводим объемы через пробел
         for (Bubble bubble : bubbles) {
-            System.out.print(bubble.geVolume() + " ");
+            System.out.print(bubble.getVolume() + " ");
         }
         System.out.println();
 
         // Итерируемся по списку и выводим имена через пробел
         for (Bubble bubble : bubbles) {
-            System.out.print(bubble.geVolume() + " ");
+            System.out.print(bubble.getName() + " ");
         }
         System.out.println();
 
         // Считаем суммарный объем всех пузырей
         int totalVolume = 0;
         for (Bubble bubble : bubbles) {
-            totalVolume += bubble.geVolume();
+            totalVolume += bubble.getVolume();
         }
         System.out.println("Total volume: " + totalVolume);
 
